2ad3fc3a80f53aba563b1462b52092ed9329c0fb
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2025-04-13 20:32:50 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,31 @@
+import ProductCard from "../components/ProductCard"
+
+const mockProducts = [
+  {
+    id: "1",
+    name: "Розовые кружевные",
+    description: "Нежные, в стиле baby-doll",
+    price: 499,
+    image: "/src/assets/imgs/pink_lace.jpg"
+  },
+  {
+    id: "2",
+    name: "Черные стринги",
+    description: "Минимум ткани — максимум страсти",
+    price: 599,
+    image: "/src/assets/imgs/black_thong.jpg"
+  }
+]
+
+const Home = () => {
+  return (
+    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {mockProducts.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default Home
 
