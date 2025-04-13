9c4b117918b5202f645b40d8e2b6c5f74e05cc0e
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2025-04-13 20:39:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
-import Cart from "./pages/Cart"
 import Product from "./pages/Product"
+import Cart from "./pages/Cart"
 import AdminPanel from "./pages/AdminPanel"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import PrivateRoute from "./components/PrivateRoute"
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow container mx-auto px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute>
+                  <AdminPanel />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
 export default App
-
